f8da49d40930a9cb59a82bf3393b5fc0bc6cb4aa
trantrungdo <trantrungdo.thaibinh@gmail.com>
2020-11-29 07:05:14 +0700

    Calculation

--- handler.js
+++ handler.js
@@ -1,5 +1,6 @@
-var MongoClient = require('mongodb').MongoClient;
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const fs = require('fs');
+var csvWriter = require('csv-write-stream');
+var writer = csvWriter({sendHeaders: false});
 
 function degrees_to_radians(degrees) {
     return degrees * (Math.PI / 180);
@@ -60,11 +61,11 @@ function match_gioitinh_type(gioitinh, type) {
         }
     }
 
-    if(gioitinh == 1) {
+    if(parseInt(gioitinh) === 1) {
         // Nam
-        return calculation["nam"][type.to_string];
+        return calculation["nam"][String(type)];
     } else {
-        return calculation["nu"][type.to_string];
+        return calculation["nu"][String(type)];
     }
 }
 
@@ -96,5 +97,27 @@ function match_interest_type(kid_interest, gift_type) {
 }
 
 function hpi (kid, gift) {
-   
+    let calculate_gioitinh_type = match_gioitinh_type(kid["gender"], gift["type"]);
+    let calculate_tuoi = match_tuoi(kid["age"], gift["ages"]);
+    let calculate_mausac = match_mausac(kid["like_color"], kid["dislike_color"], gift["color"]);
+    let calculate_interest_type = match_interest_type(kid["interest"], gift["type"]);
+    let distance = calculate_distance(kid["latitude"], kid["longitude"], gift["latitude"], gift["longitude"]);
+    let received_time = calculate_time(distance);
+
+    let result = ((calculate_gioitinh_type + calculate_tuoi + received_time/4)) * calculate_mausac * calculate_interest_type;
+
+    writer.write({kid_id: kid["id"], gift_id: gift["id"], happy_index: parseInt(result)});
 }
+
+// kid = {"id": "1516", "gender": "1", "age": "10", "interest": "10", "like_color": "White", "dislike_color": "Pink", "latitude": "61.479", "longitude": "-58.313"};
+// gift = {"id": "6501384", "type": "10", "ages": "13-15", "color": "White", "latitude": "18.704", "longitude": "110.68"};
+
+// kid1 = {"id": "1517", "gender": "0", "age": "16", "interest": "7", "like_color": "Red", "dislike_color": "Yellow", "latitude": "-82.405", "longitude": "-45.392"};
+// gift1 = {"id": "6501385", "type": "11", "ages": "4-10", "color": "Green", "latitude": "-54.504", "longitude": "-75.766"};
+
+// writer.pipe(fs.createWriteStream('./output.csv'));
+
+// hpi(kid1, gift1);
+// hpi(kid, gift);
+
+// writer.end();
